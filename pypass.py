af2751532f12dd5c349b6fa63b2e5f7f29ea1e0e
Michael Linton <michaeljlinton.ml@gmail.com>
2021-11-13 13:32:46 +0000

    Using Fernet module

--- pypass.py
+++ pypass.py
@@ -1,4 +1,7 @@
-import json
+import json, base64, os, pyperclip; 
+from cryptography.fernet import Fernet;
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC;
+from cryptography.hazmat.primitives import hashes;
 class Main:
     def __init__(self, file):
         self.file=file
@@ -18,8 +21,32 @@ class Main:
     def names(self):
         return [x['name'] for x in self.content['items']]
 
+class Crypto:
+    def __init__(self, password, salt):
+        password = bytes(password, 'utf-8')
+        salt = bytes(salt, 'utf-8')
+        kdf = PBKDF2HMAC(
+            algorithm=hashes.SHA256(),
+            length=32,
+            salt=salt,
+            iterations=100000,
+        )
+        self.key=base64.urlsafe_b64encode(kdf.derive(password))
+
+    def encrypt(self, secret):
+        secret = bytes(secret, 'utf-8')
+        f = Fernet(self.key)
+        return f.encrypt(secret)
+
+    def decrypt(self, secret):
+        f = Fernet(self.key)
+        pyperclip.copy(f.decrypt(secret).decode('utf-8'))
+        print('The text to be copied to the clipboard.')
+        
 if __name__ == '__main__':
     file=str('pypass.json')
     data=Main(file)
-    data.find(data.names[1])
-    
+    crypto=Crypto(password='password',salt='password')
+    # print(data.names)
+    # msg=crypto.encrypt('fsefsfesfesf')
+    # crypto.decrypt(msg)
